be8a7ac160b9589c0f637808a7de6bd280355fa1
David Garcia <davidgar026@gmail.com>
2024-12-17 18:57:15 -0500

    set up foundation

--- capstone_book_logger/index.js
+++ capstone_book_logger/index.js
@@ -1,3 +1,11 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+
+
+const port = 3000;
+const app = express();
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
