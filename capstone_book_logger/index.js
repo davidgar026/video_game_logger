68b1529365ea54fbc620e88e611ec15fb96e9f5e
David Garcia <david@Davids-MacBook-Pro.local>
2024-12-23 16:11:10 -0500

    worked on the authorization of a bearer token to use the IGDB API

--- capstone_book_logger/index.js
+++ capstone_book_logger/index.js
@@ -26,28 +26,65 @@ app.get("/", (req,res) => {
 })
 
 
-app.post("/get-games", async(req, res) => {
-    try{
-        const result = await axios.post("https://api.igdb.com/v4/games/?fields=name", {
-            BODY: "fields name, rating where id=302156;"
-        }, {
-            headers: {
-                'Client-ID': 't4qpnrtaj5033n4aky50zbbdjvoo79',
-                Authorization: 'Bearer bh5qbzhdnkhsagry5uje9sz7lxsf77'
-            }
-        });
 
-        /*
-        res.render("index.ejs", {
-            text: result.data,
-        })
-            */
-        console.log("result = ", result.data);
+const getAccessToken = async() => {
+    try{
+    const response = await axios.post(
+        'https://id.twitch.tv/oauth2/token', 
+        null, 
+        { params: {
+            client_id: 't4qpnrtaj5033n4aky50zbbdjvoo79',
+            client_secret: 'bkn6uhxfso0zbsu119p7x0ugf3p9nn',
+            grant_type: 'client_credentials'
+        }
+    })
+    return response.data.access_token;
+}catch(error){
+    console.error("Error fetching token: ", error.response?.data || error.message);
+}
+}
+
+
+app.post("/get-games", async(req, res) => {
+    const getToken = getAccessToken();
+
+    if(!getToken) {
+        return res.status(500).send('Unable to fetch access token.');
+    }
+
+    try{
+        const response = await axios.post('https://api.igdb.com/v4/games',
+            'fields name, genres.name, platforms.name, rating; limit 10;',
+            {
+                headers: {
+                    'Client-ID': 't4qpnrtaj5033n4aky50zbbdjvoo79',
+                    Authorization: 'Bearer 8xyzvu4tihr79a3hzz64wv6kwbmfhm'
+                }
+            }
+        );
+        console.log(response.data)
+        res.json(response.data);
     }catch(error){
-        res.status(404).send(error.response.data);
+        console.error('Error fetching data from IGDB:', error.response?.data || error.message);
+      res.status(500).send('Error fetching data from IGDB.');
     }
 });
 
+
+
+
+
+
+
+
+
+
+
+/* <----------------->*/
+
+
+  
+ 
 
 
 app.listen(port, () => {
